684715e95253b9bb413d37dcbf8635475fd28e9f
Aimin Yan <aimin.at.work@gmail.com>
2022-08-05 18:12:33 -0400

    remove blank lines

--- R/SummaryFor5andRandomIS.R
+++ R/SummaryFor5andRandomIS.R
@@ -1,8 +1,6 @@
 df.5.IS.random <- read.table('/Users/aiminyan/Aimin/aws_root/home/ubuntu/DEMO/ISseqOutput/DEMOPos5ISRandom/CutAdapt/filterNo/db/DEMOPos5ISRandom/FinalOut_DEMOPos5ISRandom/POOL-ISA-AVRO-6-Preclin_HL60POS-CTRL-1CL-6_HL60_DEMOPos5ISRandom_CollisionClean_CollisionTable.txt',header = T)
 
-
 df.5.IS.random$Relative <- df.5.IS.random$CL.6_POS.CTRL.1_1/sum(df.5.IS.random$CL.6_POS.CTRL.1_1)
-
 
 df.5.IS.random.1 <- df.5.IS.random[order(df.5.IS.random$V7),]
 
